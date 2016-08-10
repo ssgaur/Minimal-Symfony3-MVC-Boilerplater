53262ef73f90f8089136c853409246d67349d956
Shailendra Singh <shail@Shailendras-MacBook-Pro.local>
2016-08-10 15:28:02 +0530

    items conroller and dependency removed

--- app/views/items/header.php
+++
@@ -1,7 +0,0 @@
-<html>
-<head>
-<title>Title of site</title>
-
-</head>
-<body>
-<h1>This is View page</h1>
--- app/views/items/view.php
+++
@@ -1,4 +0,0 @@
-<?php
-   print_r($data);
-?>
-<h1>This is view Page staying in /app/views/view.php</h1>
